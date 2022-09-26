d120dbd8c9fafa5fc55c08ec8abe59dd1c40d825
Janggun Lee <leesisi123@naver.com>
2022-09-26 18:51:35 +0900

    Update again

--- theories/induction_fail.v
+++ theories/induction_fail.v
@@ -1,31 +1,24 @@
 From stdpp Require Import base gmultiset fin_sets.
+From Coq Require Import ssreflect.
 
 (* 공집합이 아닌 집합은 크기가 0보다 크다는 보조정리  *)
 Lemma non_empty_pos (X : gmultiset nat) n :
   n ∈ X → size X > 0.
 Proof.
   intros. assert (size X ≠ 0); [|lia].
-  intros Size.
-  apply gmultiset_size_empty_iff in Size.
-  set_solver.
+  apply gmultiset_size_non_empty_iff. multiset_solver.
 Qed.
 
-Lemma gm_empty_difference_subseteq_L (X Y : gmultiset nat) : X ∖ Y = ∅ → X ⊆ Y.
-Proof. multiset_solver. Qed.
-
-Lemma single_elem_of_gmultiset (X : gmultiset nat) n m :
+Lemma gm_size_singleton_inv (X : gmultiset nat) n m :
+  size X = 1 →
   n ∈ X → m ∈ X  →
-  1 = size X →
   n = m.
 Proof.
-  intros n_in m_in Hs.
-  apply (gmultiset_disj_union_difference' m) in m_in as Hm.
-  apply (f_equal size) in Hm. rewrite <- Hs in Hm.
-  rewrite gmultiset_size_disj_union in Hm.
-  rewrite gmultiset_size_singleton in Hm.
+  intros HX n_in m_in.
+  apply (gmultiset_disj_union_difference' m),(f_equal size) in m_in.
+  rewrite HX gmultiset_size_disj_union gmultiset_size_singleton in m_in.
   assert (X ∖ {[+ m +]} = ∅) as Hm_empty.
   { apply gmultiset_size_empty_iff. lia. }
-  apply gm_empty_difference_subseteq_L in Hm_empty.
   assert (X = {[+ m +]}) as -> by multiset_solver.
   by apply gmultiset_elem_of_singleton.
 Qed.
@@ -33,15 +26,14 @@ Qed.
 Lemma one_gmultiset_not_empty (X : gmultiset nat) n :
   size X > 1 → n ∈ X → X ∖ {[+ n +]} ≠ ∅.
 Proof.
-  intros. rewrite <-gmultiset_size_empty_iff. intros Zero.
-  rewrite gmultiset_size_difference in Zero; [|multiset_solver].
-  rewrite gmultiset_size_singleton in Zero. lia.
+  intros.
+  rewrite -gmultiset_size_empty_iff gmultiset_size_difference; [multiset_solver|].
+  rewrite gmultiset_size_singleton. lia.
 Qed.
 
 Lemma apply_induction (P : gmultiset nat) p s P' :
   (∀ (m n : nat) (P : gmultiset nat),
-    P ≠ ∅ → m ∈ P → n ∈ P → s = size P → n = m)
-  →
+    P ≠ ∅ → m ∈ P → n ∈ P → s = size P → n = m) →
   s > 0 →
   S s = size P →
   p ∈ P →
@@ -50,7 +42,7 @@ Lemma apply_induction (P : gmultiset nat) p s P' :
 Proof.
   intros IHs' ?? p_in HP'.
   assert (s = size P').
-  { apply (f_equal size) in HP'. rewrite gmultiset_size_difference in HP'; [|multiset_solver].
+  { apply (f_equal size) in HP'. rewrite gmultiset_size_difference in HP'; [multiset_solver|].
     rewrite gmultiset_size_singleton in HP'. lia.
   }
   destruct (gmultiset_choose P') as [m m_in].
@@ -65,22 +57,22 @@ Proof.
   destruct (gmultiset_choose P) as [m m_in]; [done|].
   exists m.
   (* n ∈ X라는 사실과 귀납변수가 될 집합의 크기 m을 소개. *)
-  intro n_in. remember (size P) as s eqn:Hs.
+  intro n_in. remember (size P) as N eqn:HN.
   (* 보조정리로 크기가 0보다 크다는 사실을 암. *)
-  assert (s > 0) as s_pos by (subst s; by apply (non_empty_pos P n)).
+  assert (N > 0) as N_pos by (subst N; by apply (non_empty_pos P n)).
   (* 올바른 induction hypothesis을 얻기 위한 작업. *)
   generalize dependent P. generalize dependent n. generalize dependent m.
   (* 귀납 시작. *)
-  induction s as [|s' IHs'].
+  induction N as [|N' IHN'].
   - (* Base Case. Vacuously true. *)
-    inversion s_pos.
+    inversion N_pos.
   - (* Inductive Case. *)
-    destruct (decide (s' = 0)) as [->|Gt]; intros m n P NotEmpy m_in n_in Hs'.
-    (* 우선 s' = 0일때를 후딱 치우자. *)
-    { by apply (single_elem_of_gmultiset P n m). }
-    (* 그럼 s' > 0이다. *)
-    assert (s' > 0) as Pos by lia;
-    specialize (IHs' Pos). clear Pos s_pos.
+    (* 우선 N = 1일때를 해결하자. *)
+    destruct (decide (N' = 0)) as [->|Gt]; intros m n P NotEmpy m_in n_in HN'.
+    { apply (gm_size_singleton_inv P n m); [lia|done..]. }
+    (* 그럼 N > 1이다. *)
+    assert (N' > 0) as Pos by lia;
+    specialize (IHN' Pos). clear Pos N_pos.
 
     (* 우선 p1을 얻기 위해 공집합이 아님을 보임 *)
     assert (P ≠ ∅) as NotEmpty.
@@ -91,7 +83,7 @@ Proof.
     (* [P ∖ {[+ p1 +]}]에 대해 IHs' 적용. *)
     remember (P ∖ {[+ p1 +]}) as P1 eqn:HP1.
     assert (∃ m1, ∀ (n1 : nat), n1 ∈ P1 → n1 = m1) as P1_all_same.
-    { apply (apply_induction P p1 s'); auto with lia. }
+    { apply (apply_induction P p1 N'); auto with lia. }
 
     (* 이제 다른 원소 p2를 얻어야함 *)
     assert (P ∖ {[+ p1 +]} ≠ ∅) as NotEmpty.
@@ -103,7 +95,7 @@ Proof.
     (* [P ∖ {[+ p2 +]}]에 대해 IHs' 적용. *)
     remember (P ∖ {[+ p2 +]}) as P2 eqn:HP2.
     assert (∃ m2, ∀ (n2 : nat), n2 ∈ P2 → n2 = m2) as P2_all_same.
-    { apply (apply_induction P p2 s'); auto with lia. multiset_solver. }
+    { apply (apply_induction P p2 N'); auto with lia. multiset_solver. }
 
     (* 교집합에서 원소가 있음을 보이기 위해, 교집합이 공집합이 아님을 증명해보자... *)
     assert (P1 ∩ P2 ≠ ∅) as NonEmpty.
